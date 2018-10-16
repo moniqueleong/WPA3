8823e2d2542dd9353025f736fd2a0873bb5296d5
Monique Leong <37385429+s3722811@users.noreply.github.com>
2018-10-16 20:13:43 +1100

    Add files via upload

---
+++ receipt.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang='en'>
+
+<head>
+    <meta charset="utf-8">
+    <title>Assignment 3</title>
+
+    <!-- Keep wireframe.css for debugging, add your css to style.css -->
+    <link id='wireframecss' type="text/css" rel="stylesheet" href="../wireframe.css" disabled>
+    <link id='stylecss' type="text/css" rel="stylesheet" href="css/style.css">
+    <script src='../wireframe.js'></script>
+	<style>
+
+</style>
+</head>
+    <header>
+        <h1><img src="../../media/logo.png" style="height:100px"></h1>
+
+    </header>
+
+    <nav>
+             <li><a href="index.php">Home</a></li>
+        <li><a href="login.php">Log in</a></li>
+        <li><a href="products.php">Products</a></li>
+        <li><a href="product.php">Product</a></li>
+        <li style="float:right"><a href="cart.php">Cart</a></li>
+
+		
+
+    </nav>
+
+    <main>
+        <h2> Receipt </h2>
+    </main>
+	<body>
+	
+   
+	  </body>
+
+    <footer>
+        <div>&copy;
+            <script>
+                document.write(2018);
+
+            </script> Monique Leong / Yousef Fares, s3722811 / s3724131</div>
+        <div>Disclaimer: This website is not a real website and is being developed as part of a School of Science Web Programming course at RMIT University in Melbourne, Australia.</div>
+        <div><button id='toggleWireframeCSS' onclick='toggleWireframe()'>Toggle Wireframe CSS</button></div>
+    </footer>
+
+</body>
+
+</html>
